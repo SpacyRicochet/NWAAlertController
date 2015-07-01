6d144579646ab919e88bd16cc18984cc6db28422
Bruno Scheele <bruno@noodlewerk.com>
2015-07-01 12:54:04 +0200

    Update test case for rdar://21613035

--- Example/NWAAlertController/ViewController.swift
+++ Example/NWAAlertController/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 import NWAAlertController
 
 class ViewController: UIViewController {
+    @IBOutlet weak var stackView: UIStackView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("UIStackView layoutMargins: \(stackView.layoutMargins) and layoutMarginsRelativeArrangement: \(stackView.layoutMarginsRelativeArrangement)")
+//        stackView.layoutMargins = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+//        stackView.layoutMarginsRelativeArrangement = true
+//        print("UIStackView layoutMargins: \(stackView.layoutMargins) and layoutMarginsRelativeArrangement: \(stackView.layoutMarginsRelativeArrangement)")
+        
     }
 
     override func didReceiveMemoryWarning() {
